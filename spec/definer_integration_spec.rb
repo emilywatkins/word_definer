06e7c62c82f7190a3029cf82b83af389e162ae7b
Emily Watkins <emily.e.dolliver@gmail.com>
2018-02-16 10:11:56 -0800

    add failing integration spec to track showing words to list, add submit button.

--- spec/definer_integration_spec.rb
+++ spec/definer_integration_spec.rb
@@ -2,3 +2,12 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('the word path', {:type => :feature}) do
+  it('processes the user entry and returns on a new page with that word') do
+    visit('/')
+    fill_in('word', :with => 'squirrel')
+    click_button('Submit')
+    expect(page).to have_content('squirrel')
+  end
+end
