0efb0c85aa93caf138f956b076708b4bf34cc149
Emily Watkins <emily.e.dolliver@gmail.com>
2018-02-16 09:28:50 -0800

    add failing test to check for 'all' method to start out with empty array.

--- lib/definer.rb
+++ lib/definer.rb
@@ -1,3 +1,14 @@
 class Word
+  @@word_list = []
+
+  attr_reader :word
+
+  def initialize(attributes)
+    @word = attributes.fetch(:word)
+  end
+
+  def self.all
+
+  end
 
 end
--- spec/definer_spec.rb
+++ spec/definer_spec.rb
@@ -2,3 +2,14 @@ require("sinatra")
 require("rspec")
 require("pry")
 require("definer")
+
+describe('Word') do
+
+  describe('.all') do
+    it('is empty at first') do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+
+end
